2f6d9a670c0ebe4466fca01bfe279814dcb3aa96
Marcos-SCO <marcos_sco@outlook.com>
2022-10-22 03:19:44 -0300

    refactor: afastamento do menu

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,1 +1,15 @@
-import './swiper';
+function navBarPaddingCorrection() {
+  let $mainNav = document.querySelector('[data-js="mainNav"]');
+  let $mainHeader = document.querySelector('[data-js="mainHeader"]');
+  if (!$mainNav || !$mainHeader) return;
+
+  // Função para que o container principal se afaste do menu
+  ['DOMContentLoaded', 'resize'].forEach(event => {
+    window.addEventListener(event, function (event) {
+      $mainHeader.style = 'padding-top: ' +  $mainNav.offsetHeight + 'px';
+    });
+  });
+
+}
+
+document.addEventListener('DOMContentLoaded', navBarPaddingCorrection);
